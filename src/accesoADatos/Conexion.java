544460f3b16b0c7c080d2c1026c715eaad120851
mateo2270 <40643674+mateo2270@users.noreply.github.com>
2023-10-09 19:00:14 -0300

    fffffff

--- src/accesoADatos/Conexion.java
+++ src/accesoADatos/Conexion.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 public class Conexion {
     
     private static final String URL = "jdbc:mariadb://localhost/";
-    private static final String BD = "biblioteca_grupo14";
+    private static final String BD = "gg3";
     private static final String USUARIO= "root";
     private static final String PASSWORD= "";
     private static Connection connection;
